2f3b4acbbd6fd20cb17267d537095d33dd7fd3a1
gkcksrbs <70510732+gkcksrbs@users.noreply.github.com>
2020-11-19 16:04:53 +0900

    2020-10-06

--- 동적 계획법 1/Baekjoon_2748_피보나치 수 2.py
+++ 동적 계획법 1/Baekjoon_2748_피보나치 수 2.py
@@ -1,13 +1,20 @@
 import sys
-
-N = int(sys.stdin.readline())
-
+# n 번째 피보나치 수를 출력받기 위한 n 입력
+n = int(sys.stdin.readline())
+# DP 테이블 초기화
+d = [-1]*(n+1)
+d[0], d[1] = 0, 1
+# 다이나믹 프로그래밍 함수(피보나치 수열)
 def Fibonacci(n):
-    if(n == 0):
-        return 0
-    if (n == 1):
-        return 1
+    # n 번째 DP 테이블이 -1이 아니라면
+    if d[n] != -1:
+        # 해당 인덱스의 DP 테이블 값 반환
+        return d[n]
+    # n 번쨰 DP 테이블이 -1이라면
     else:
-        return Fibonacci(n-2) + Fibonacci(n-1)
-
-print(Fibonacci(N))
+        # 피보나치 함수 재귀 실행
+        d[n] = Fibonacci(n-1) + Fibonacci(n-2)
+        # 해당 인덱스의 DP 테이블 값 반환
+        return d[n]
+# 피보나치 함수 실행 및 결과값 출력
+print(Fibonacci(n))
