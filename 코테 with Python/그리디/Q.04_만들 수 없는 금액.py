8a214953a9cb41b64bb4d73de980edbd457a2b83
gkcksrbs <70510732+gkcksrbs@users.noreply.github.com>
2020-12-13 22:21:28 +0900

    2020-10-06

--- 코테 with Python/그리디/Q.04_만들 수 없는 금액.py
+++ 코테 with Python/그리디/Q.04_만들 수 없는 금액.py
@@ -1,22 +1,40 @@
-from itertools import combinations
+# 비효율
+# from itertools import combinations
+#
+# N = int(input())
+# coin = list(map(int, input().split()))
+# lst = []
+#
+# for i in range(1, N+1):
+#     for j in combinations(coin, i):
+#         lst.append(sum(j))
+#
+# lst = list(set(lst))
+# last = lst[len(lst)-1]
+# Min = 0
+#
+# for i in range(1, last+1):
+#     if i not in lst:
+#         Min = i
+#
+# if min == 0:
+#     print(last+1)
+# else:
+#     print(Min)
 
-N = int(input())
-coin = list(map(int, input().split()))
-lst = []
-
-for i in range(1, N+1):
-    for j in combinations(coin, i):
-        lst.append(sum(j))
-
-lst = list(set(lst))
-last = lst[len(lst)-1]
-Min = 0
-
-for i in range(1, last+1):
-    if i not in lst:
-        Min = i
-
-if min == 0:
-    print(last+1)
-else:
-    print(Min)
+import sys
+# 입력 빠르게 하기 위한 변수
+input = sys.stdin.readline
+# 동전의 개수 입력
+n = int(input())
+# 각 동전의 화폐 단위 입력
+data = list(map(int, input().split()))
+# 결과값
+target = 1
+# 모든 동전 탐색
+for x in data:
+    if target < x:
+        break
+    target += x
+# 결과값 출력
+print(target)
